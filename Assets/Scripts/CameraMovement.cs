90f8c0dafba3a1145452fde7b4a8b4c3977f513e
lmartinignacio <lmartinignacio@gmail.com>
2022-02-25 00:31:02 -0300

    The floor is lava scene

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float forwardSpeed = -2.0f;
+    [SerializeField] float rightSpeed = 2.0f;
+    [SerializeField] float rotationSpeed = -5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.forward * forwardSpeed * Time.deltaTime;
+        transform.localPosition += (Vector3.right * rightSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
